7c75ea190ad543d1677b4459b92a1dbd16a4b90c
yihaoliu <liuyihao@krspace.cn>
2018-08-28 16:16:57 +0800

    feat:ok

--- build/automaticRoute/index.js
+++ build/automaticRoute/index.js
@@ -2,9 +2,12 @@ const render = require('./generateRoute');
 const chokidar = require('chokidar');
 const path = require('path');
 const templateFile = chokidar.watch(path.join(__dirname, '../../pages'));
-
+const env = process.env.NODE_ENV 
 function auto() {
     render()
+    if(env === 'production'){
+        return ;
+    }
     templateFile.on('ready', () => {
         templateFile.on('add', (path) => {
             autoRoute();
