0f4707e0c9a3495781751805cbbe568532bd72d2
yihaoliu <liuyihao@krspace.cn>
2018-09-07 17:47:48 +0800

    chore: 搜索ok

--- config/index.js
+++ config/index.js
@@ -15,8 +15,8 @@ module.exports = {
 
     // Various Dev Server settings
     // host: 'adminlocal.krspace.cn', // can be overwritten by process.env.HOST
-    host:'127.0.0.1',
-    port: 8001, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    host:'adminlocal.krspace.cn',
+    port: 8082, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
