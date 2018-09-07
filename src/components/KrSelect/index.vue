d15d252f82c9df5f276b933a64fd2f0c90aeed36
yihaoliu <liuyihao@krspace.cn>
2018-09-07 10:12:19 +0800

    chore: 搜索ok

--- src/components/KrSelect/index.vue
+++ src/components/KrSelect/index.vue
@@ -70,7 +70,7 @@ export default {
     computed:{
         modelValue: {
             get() {
-                return this.value
+                return ''+this.value
             },
             set(val) {
 
@@ -87,8 +87,8 @@ export default {
     },
     methods: {
         changeContent(value) {
-            this.$emit('input', value) // 自定义组件双向绑定的关键 向外发布input事件 事件名为model中 设置的event的属性值 可自定义其他名称 
-            this.$emit('on-select-change', value)// 向外发布 on-select-change 事件
+            this.$emit('input', ''+value) // 自定义组件双向绑定的关键 向外发布input事件 事件名为model中 设置的event的属性值 可自定义其他名称 
+            this.$emit('on-select-change', ''+value)// 向外发布 on-select-change 事件
         },
         getCusomerList() {
             let params = Object.assign({},this.params);
