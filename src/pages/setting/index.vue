9053b288a47a80addc97171bafb414ed9d2cc45c
yihaoliu <liuyihao@krspace.cn>
2018-09-12 17:05:00 +0800

    feat: 到场人数

--- src/pages/setting/index.vue
+++ src/pages/setting/index.vue
@@ -243,19 +243,19 @@
             detail(index){
                 //this.$router.push({path:'/settingDetail',query:{id:this.data1[index].id}})
                 //window.open(window.location.origin+"/#/settingDetail?id="+this.data1[index].id); 
-                 window.open("admin-applet/#/settingDetail?id="+this.data1[index].id); 
+                 window.open("/#/settingDetail?id="+this.data1[index].id); 
             },
             edit(index){
                 //this.$router.push({path:'/editGoods',query:{id:this.data1[index].id}})
                 // window.open(window.location.origin+"/#/editGoods?id="+this.data1[index].id); 
-                 window.open("admin-applet/#/editGoods?id="+this.data1[index].id); 
+                 window.open("/#/editGoods?id="+this.data1[index].id); 
             },
             newBuiltGoods(){
                  //this.$router.push({path:'/newBuiltGoods'})
                 //  console.log('window.location.origin')
                 //  console.log(window.location.origin+"/#/newBuiltGoods")
                 //window.open(window.location.origin+"/#/newBuiltGoods"); 
-                window.open("admin-applet/#/newBuiltGoods"); 
+                window.open("/#/newBuiltGoods"); 
                  //window.open( window.location.origin+"/newBuiltGoods"); 
             },
         }
